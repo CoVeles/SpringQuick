fc9cf4df37c7243201cede26c9176ff4d1968f71
coveles <plotbug@gmail.com>
2025-10-27 19:00:16 +0200

    Connect beans by adding parameter to the calling bean

--- sq-ch3-ex1/src/main/java/org/example/config/ProjectConfig.java
+++ sq-ch3-ex1/src/main/java/org/example/config/ProjectConfig.java
@@ -15,10 +15,10 @@ public class ProjectConfig {
     }
 
     @Bean
-    public Person person () {
+    public Person person (Parrot parrot ) {
         Person person = new Person();
         person.setName("Pojo");
-        person.setParrot(parrot());
+        person.setParrot(parrot);
         return person;
     }
 }
