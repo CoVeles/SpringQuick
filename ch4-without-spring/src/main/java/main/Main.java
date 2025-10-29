97a21876a9cc995e1934ef75a20a7635ed02dd80
coveles <plotbug@gmail.com>
2025-10-29 12:23:52 +0200

    DI through Abstractions(with Spring and @Component)

--- ch4-without-spring/src/main/java/main/Main.java
+++ ch4-without-spring/src/main/java/main/Main.java
@@ -1,21 +1,19 @@
 package main;
 
+import configuration.ProjectConfiguration;
 import model.Comment;
-import proxies.CommentNotificationProxy;
-import proxies.EmailCommentNotificationProxyImpl;
-import repositories.CommentRepository;
-import repositories.DBCommentRepositoryImpl;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import services.CommentService;
 
 public class Main {
     public static void main(String[] args) {
-        CommentRepository cr = new DBCommentRepositoryImpl();
-        CommentNotificationProxy cnp = new EmailCommentNotificationProxyImpl();
-        CommentService cs = new CommentService(cr,cnp);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfiguration.class);
 
         Comment comment = new Comment();
         comment.setText("Hello World");
         comment.setAuthor("John Doe");
+
+        CommentService cs = context.getBean(CommentService.class);
 
         cs.publishComment(comment);
     }
--- ch4-without-spring/src/main/java/repositories/DBCommentRepositoryImpl.java
+++ ch4-without-spring/src/main/java/repositories/DBCommentRepositoryImpl.java
@@ -1,7 +1,9 @@
 package repositories;
 
 import model.Comment;
+import org.springframework.stereotype.Component;
 
+@Component
 public class DBCommentRepositoryImpl implements CommentRepository {
     @Override
     public void storeComment(Comment comment) {
